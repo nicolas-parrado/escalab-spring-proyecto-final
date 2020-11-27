a3eccc6dc2185d2285a241eef729786dd8a43b62
Nicolás Parrado <nparrado@MacBook-Pro-de-Nicolas.local>
2020-11-27 16:56:19 -0300

    Esqueleto de aplicación Spring + README

---
+++ backend-spring/src/test/java/escalab/spring/nparrado/backend_spring/BackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package escalab.spring.nparrado.backend_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
